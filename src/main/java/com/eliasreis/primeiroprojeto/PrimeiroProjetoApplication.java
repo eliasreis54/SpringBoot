baa580048a9a90594f97fdc7dec3217793c9a81d
Elias Reis <eliasreis54@gmail.com>
2018-01-13 00:19:31 -0200

    Mais um teste do travisCI

--- src/main/java/com/eliasreis/primeiroprojeto/PrimeiroProjetoApplication.java
+++ src/main/java/com/eliasreis/primeiroprojeto/PrimeiroProjetoApplication.java
@@ -9,5 +9,6 @@ public class PrimeiroProjetoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PrimeiroProjetoApplication.class, args);
 		System.out.println("Hello wold");
+		System.out.println("Testing TravisCI");
 	}
 }
